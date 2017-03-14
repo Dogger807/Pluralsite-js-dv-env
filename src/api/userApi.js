2b103af5c0c6fc993734db2cd9ec74cb42913cf5
Derrick <derrick@clientcpr.com>
2017-03-13 22:56:29 -0400

    God Damn Mother fucking son of a bitch

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -2,15 +2,30 @@ import 'whatwg-fetch';
 import getBaseUrl from './baseUrl';
 
 const baseUrl = getBaseUrl();
-console.log ('baseUrl  = ' + baseUrl);// eslint-disable-line no-console
+// console.log ('baseUrl  = ' + baseUrl);// eslint-disable-line no-console
 
 export function getUsers() {
   return get('users');
 }
 
+export function deleteUser(id) {
+  return del(`users/${id}`);
+}
+
 function get(url) {
   return fetch(baseUrl + url).then(onSuccess, onError);
 }
+
+// Can't call func delete since reserved word.
+function del(url) {
+  const request = new Request(baseUrl +url , {
+    method: 'DELETE'
+  });
+
+  return fetch(request).then(onSuccess, onError);
+}
+
+
 
 function onSuccess (response) {
   return response.json();
